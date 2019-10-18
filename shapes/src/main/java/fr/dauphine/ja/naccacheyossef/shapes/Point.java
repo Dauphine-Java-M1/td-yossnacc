67fdc4f966cf5c0081ecc26af28ad0b39362c1af
Yossef <yinaccache@hotmail.fr>
2019-10-18 13:16:39 +0200

    Finish ring class

--- shapes/src/main/java/fr/dauphine/ja/naccacheyossef/shapes/Point.java
+++ shapes/src/main/java/fr/dauphine/ja/naccacheyossef/shapes/Point.java
@@ -1,5 +1,7 @@
 package fr.dauphine.ja.naccacheyossef.shapes;
 
+import java.util.ArrayList;
+
 /**
  * Hello world!
  *
@@ -17,11 +19,39 @@ public class Point
 		this.nb = nb+1;
 	}
 	
+	public void translate(int dx, int dy) {
+		this.x += dx;
+        this.y += dy;
+	}
+	
     public static void main( String[] args )
     {
-    	Point p = new Point(5,6);
+    	/*Point p = new Point(5,6);
     	System.out.println(p);
-    	System.out.println(nb);
+    	System.out.println(nb);*/
+    	
+    	/*Point p1=new Point(1,2);
+    	Point p2=p1;
+    	Point p3=new Point(1,2);
+    	
+    	ArrayList<Point> list = new ArrayList<Point>();
+    	list.add(p1);
+    	System.out.println(list.indexOf(p2));
+    	System.out.println(list.indexOf(p3));*/
+    	
+    	//boolean test = isSameAs(p1);
+    	//System.out.println(test);
+    	
+    	/*Point p=new Point(1,2);
+    	Circle c=new Circle(p,1);
+    	Circle c2=new Circle(p,2);
+    	c2.translate(1,1);
+    	System.out.println(c+" "+c2);*/
+    	
+    	Circle c=new Circle(new Point(1,2), 1);
+    	c.getCenter().translate(1,1);
+    	System.out.println(c);
+
 
     }
 
@@ -46,5 +76,16 @@ public class Point
 		return "(" + x + "," + y + ")";
 	}
 	
+	public static boolean isSameAs(Point p) {
+		return (p.x == p.y) ? true : false ;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if(!(obj instanceof Point)) return false;
+		Point p = (Point) obj;
+		if ((x != p.x) || (y != p.y)) return false;
+		return true;
+	}
 	
 }
